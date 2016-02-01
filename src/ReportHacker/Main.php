b91ecf605f767bf52ead6ed233c0102de286e2d2
applqpak <applqpak@gmail.com>
2016-02-02 10:57:12 +1100

    Create Main.php

---
+++ src/ReportHacker/Main.php
@@ -0,0 +1,62 @@
+<?php
+
+  namespace ReportHacker;
+
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\Player;
+  use pocketmine\command\Command;
+  use pocketmine\command\CommandSender;
+
+  class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+
+      if(strtolower($cmd->getName()) === "reporthacker") {
+
+        if(!(isset($args[0]))) {
+
+          $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /reporthacker <player>");
+
+          return true;
+
+        } else {
+
+          $sender_name = $sender->getName();
+
+          $sender_display_name = $sender->getDisplayName();
+
+          $name = $args[0];
+
+          $player = $this->getServer()->getPlayer($name);
+
+          if($player === null) {
+
+            foreach($this->getServer()->getOnlinePlayers() as $p) {
+
+              if($p->isOp()) {
+
+                $p->sendMessage(TF::YELLOW . $sender_name . " reported " . $name . " for using hacks/mods!");
+
+              }
+
+            }
+
+            return true;
+
+          } else {
+
+            foreach($this->getServer()->getOnlinePlayers() as $p) {
+
+              if($p->isOp()) {
+
+                $p->sendMessage(TF::YELLOW . $sender_name . " reported " . $name . " for using hacks/mods!");
+
+              }
